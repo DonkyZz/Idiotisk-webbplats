ec86b2cd587606b8826203a10464a2b0222d65f7
Adde <Adde_eriksson@hotmail.se>
2017-05-28 17:30:56 +0200

    kul skoj
    .

---
+++ include.php
@@ -0,0 +1,12 @@
+	<img src="images/Gras_img_Juni.jpg">
+
+	<a href="index.html"><img class="logga" src="images/graslogga.jpg"></a>
+	
+	<ul>
+		<li>
+			<a href="GrasInfo.html">GräsInfo</a>
+			<a href="GrasKlippare.html"> Gräsklippare</a> 
+			<a href="Synonymer.html"> Synonymer</a>
+			<a href="bilder.html"> Bilder</a>
+		</li>	
+	</ul>
